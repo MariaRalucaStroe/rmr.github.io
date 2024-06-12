2e4317d384121a38180ec9c0c518ddd2560c4f7c
Raluca <69381352+ralucacodes@users.noreply.github.com>
2024-06-12 17:04:33 +0200

    Create hello.py

---
+++ python/GUI Projects/hello.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root =Tk()
+
+#create something: a label Widget
+myLabel= Label(root, text="Hello and welcome!")
+
+#put it onto the screen
+myLabel.pack()
+
+root.mainloop()
